faa62a73ac652952fe24ef4de286347d55ac08ae
thalistarr <thalita.souza@gamersclub.com.br>
2022-05-31 20:36:28 -0300

    lesson 1

---
+++ js_001.js
@@ -0,0 +1,22 @@
+/*tipos de variáveis*/
+
+let nome = 'thalistarr'
+let ano_nascimento = 1988
+let agora = new Date()
+let idade = agora.getFullYear() - ano_nascimento
+
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos de idade.")
+/*console.log(agora)
+console.log(Date())*/
+
+/*estruturas de decisão*/
+if(idade >= 18){
+	console.log("Parabéns, você é maior de idade e está apto a dirigir")
+  if (idade >20){
+  console.log("Você pode ter um carro 0K")
+  } else {
+  console.log("Você só pode ter um carro usado")
+  }
+} else {
+	console.log("Infelizmente você ainda não está apto a dirigir, pois é menor de idade")
+}

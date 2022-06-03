9a657aa372967b46817e4c33fd1bef89c9a15ae0
thalistarr <thalita.souza@gamersclub.com.br>
2022-06-02 21:06:33 -0300

    building pokedex

--- index.js
+++ index.js
@@ -14,16 +14,33 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
     })
     .then(async data => {
   	    const box = document.querySelector('#pokemon-box')
-  	    box.querySelector('#pokemon-name')
-        console.log(box)
-  	    //console.log(data.results)
+  	    page.innerHTML = ''
   
         for(let i = 0; i < data.results.length; i++) {
             box.querySelector('#pokemon-name').innerHTML = data.results[i].name
-            box.querySelector('#pokemon-name').style.textTransform = "capitalize"
+            box.querySelector('#pokemon-name').style.textTransform = 'capitalize'
+            box.querySelector('#pokemon-name').style.color = "#660066"
+            box.querySelector('#pokemon-name').style.fontStyle = "bold"
+            box.querySelector('#pokemon-name').style.fontSize = "24"
+
+            const pokemonType = await fetch ('https://pokeapi.co/api/v2/pokemon/' + data.results[i].name)
+            const type = await pokemonType.json()
+            box.querySelector('#pokemon-type').innerHTML = 'type:' + type.types[0].type.name
+            box.querySelector('#pokemon-type').style.color = "#9494b8"
+            box.querySelector('#pokemon-type').style.fontStyle = "oblique"
+            box.querySelector('#pokemon-type').style.fontSize = "16"
+
+            const pokemonSpecie = await fetch ('https://pokeapi.co/api/v2/pokemon/' + data.results[i].name)
+            const specie = await pokemonSpecie.json()
+            box.querySelector('#pokemon-specie').innerHTML = 'specie:' + type.types[0].type.name
+            box.querySelector('#pokemon-specie').style.color = "#9494b8"
+            box.querySelector('#pokemon-specie').style.fontStyle = "oblique"
+            box.querySelector('#pokemon-specie').style.fontSize = "16"
+
             const pokemonImage = await fetch('https://pokeapi.co/api/v2/pokemon-form/' + data.results[i].name)
             const image = await pokemonImage.json()
             box.querySelector('#pokemon-img').src = image.sprites.front_default
+
             page.innerHTML += box.outerHTML
         }
     })
